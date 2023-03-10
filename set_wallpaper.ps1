a73c0c38160f54135cbdda58aa16dd9d415f105d
spinuu <45002869+spinuu@users.noreply.github.com>
2023-03-10 13:37:42 +0100

    Create set_wallpaper.ps1

---
+++ set_wallpaper.ps1
@@ -0,0 +1,12 @@
+function Set-WallPaper($Value)
+
+{
+
+ Set-ItemProperty -path 'HKCU:\Control Panel\Desktop\' -name wallpaper -value $value
+
+ rundll32.exe user32.dll, UpdatePerUserSystemParameters
+
+}
+
+copy-item "\\remotelocation\wallpaper.png" -Destination "c:\users\$env:username\documents\"
+Set-WallPaper -value "c:\users\$env:username\documents\wallpaper.png"
