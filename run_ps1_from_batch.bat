b5031b0b98b205f97a4f43971f9f96283e648d73
spinuu <45002869+spinuu@users.noreply.github.com>
2023-03-10 14:05:59 +0100

    Update run_ps1_from_batch.bat

--- run_ps1_from_batch.bat
+++ run_ps1_from_batch.bat
@@ -1,3 +1,4 @@
+REM ze względów bezpieczeństwa nie zmieniam ustawień PS na stałe tylko odpalam skrypty tym batchem
 @echo off
 Powershell.exe -executionpolicy remotesigned -File "C:\script.ps1"
 pause
